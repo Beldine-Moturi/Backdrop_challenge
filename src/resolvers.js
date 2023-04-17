2e3a7685a9f3c2966ad68858576b4c6ff9a83ad1
Beldine-Moturi <beldinemoturi@gmail.com>
2023-04-18 01:32:57 +0300

    implement query that returns user account name

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,11 +1,23 @@
 import { users } from "./db"
 // Fetch data from postreSQL database
+const request = require('request');
 
 const resolvers = {
     Query: {
         user: (parent, { bankCode, accNumber }, context, info) => {
-            return users.find(user => user.bankCode == bankCode && user.accountNumber == accNumber);
-            //return user.accountName;
+            // call paystack API to get account name -- return this if accName not in our database
+            const user = users.find(user => user.bankCode == bankCode && user.accountNumber == accNumber);
+            if (user && user['accountName']) {
+                return user['accountName'];
+            } else {
+                const accName = getPaystackName(accNumber, bankCode);
+
+                if (user && user['isVerified'] && levenshteinDistance(user['accountName'], accName) <= 2) {
+                    return user['accountName'];
+                } else {
+                    return accName;
+                }
+            }
         }
     },
     Mutation: {
@@ -23,5 +35,30 @@ const resolvers = {
 };
 
 // write function to do the Levenshtein Distance calculation
+function levenshteinDistance(userName, accName) {
+    // TODO
+}
+
+// write function that makes the paystack API calls and returns account name
+function getPaystackName(accNumber, bankCode) {
+    const paystackAPIKey = process.env.PAYSTACK_API_KEY;
+    const options = {
+        url: `https://api.paystack.co/bank/resolve?account_number=${accNumber}&bank_code=${bankCode}`,
+        headers: {
+            "Authorization": `Bearer ${paystackAPIKey}`
+        }
+    };
+
+    request(options, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body);
+            return data['data']['account_name'];
+        } else {
+            return null;
+        }
+    });
+
+}
+
 
 export default resolvers;
